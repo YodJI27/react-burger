423a3e4cbe87bf3bbbc1be67b93a664c34770b2b
YodJI27 <yodji2308@mail.ru>
2025-02-21 15:02:28 +0300

    правка ответа от сервера

--- utils/burgerApi.ts
+++ utils/burgerApi.ts
@@ -1,5 +1,5 @@
 export const BASE_URL = "https://norma.nomoreparties.space/api";
 
-export const checkResponse = (res: any) => {
+export const checkResponse = (res: Response) => {
     return res.ok ? res.json() : res.json().then((err: any) => Promise.reject(err));
 }
